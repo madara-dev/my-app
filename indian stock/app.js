e6d39ea560399b918877b01a2f42a13282eb2bc9
Madara_Dev <87358674+madara-dev@users.noreply.github.com>
2023-04-19 17:52:47 +0530

    Delete indian stock directory

--- indian stock/app.js
+++
@@ -1,9 +0,0 @@
-import { NseIndia } from  "stock-nse-india";
-const  nseIndia = new  NseIndia()
-// To get equity details for specific symbol
-nseIndia.getEquityDetails('IRCTC').then(details  => {
-console.log(details.priceInfo.lastPrice)
-})
- 
-// To get equity historical data for specific symbol
-
